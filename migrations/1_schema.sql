72d4a7be50827fede0c05fe6289781a2c4f50644
bfahrenfort <bfahrenfort1@gmail.com>
2024-04-16 10:44:31 -0500

    fix: migrations should reflect default scaffolding

--- migrations/1_schema.sql
+++ migrations/1_schema.sql
@@ -3,7 +3,5 @@ DROP TABLE IF EXISTS entries;
 CREATE TABLE IF NOT EXISTS entries (
   id serial PRIMARY KEY,
   name TEXT NOT NULL,
-  doctype TEXT NOT NULL,
-  url TEXT,
-  UNIQUE (name, doctype)
+  UNIQUE (name)
 );
